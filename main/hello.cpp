6a112a212902c9bc29bf3a476b9aa873df85e611
procub3r <sripradp@proton.me>
2025-03-14 15:35:20 +0530

    Second commit

--- main/hello.cpp
+++ main/hello.cpp
@@ -7,5 +7,6 @@ int main(void) {
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run this  \n";
   cout << "I am PES2UG22CS393. This lab has been done honestly without the use of AI or others' work\n";
+  cout << "\n--- EDITED FOR SECOND COMMIT ---\n";
   return 0;
 }
