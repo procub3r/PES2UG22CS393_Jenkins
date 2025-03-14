deb2a3f769d9569c4cb064ed61786a1be8c27fce
procub3r <sripradp@proton.me>
2025-03-14 15:12:45 +0530

    Initial commit

---
+++ main/hello.cpp
@@ -0,0 +1,11 @@
+//File: hello.cpp
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  cout << "Hello, World\n";
+  cout << "Hello, Jenkins\n";
+  cout << "I have successfully built and run this  \n";
+  cout << "I am PES2UG22CS393. This lab has been done honestly without the use of AI or others' work\n";
+  return 0;
+}
